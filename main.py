52678268e886009f46f41be00e8bf826973cfab4
GuochuYee <94251855+GuochuYee@users.noreply.github.com>
2022-02-10 07:49:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ data_dir = "./dataset"
 checkpoints_dir = "/cache/checkpoints"
 
 def adjust_learning_rate(optimizer, epoch, args):
-    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    """Sets the learning rate to the initial LR decayed by 10 every 60 epochs"""
     lr = args.lr * (0.1 ** (epoch // 60))
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
